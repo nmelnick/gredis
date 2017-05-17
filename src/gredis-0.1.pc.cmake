9bbc3280b9c65b127f6ea9ae3395b9fd9f29a2ef
Nicholas Melnick <nick@abstractwankery.com>
2017-05-17 17:54:45 -0500

    Fix pc file to claim hiredis/event deps

--- src/gredis-0.1.pc.cmake
+++ src/gredis-0.1.pc.cmake
@@ -7,6 +7,6 @@ includedir=@DOLLAR@{prefix}/include
 Name: @PKGNAME@-@PKGVERSION@
 Description: Redis library for Vala based on hiredis.
 Version: @PKGVERSION@
-Libs: -L@DOLLAR@{libdir} -l@PKGNAME@-@PKGVERSION@
+Libs: -L@DOLLAR@{libdir} -l@PKGNAME@-@PKGVERSION@ -levent -lhiredis
 Cflags: -I@DOLLAR@{includedir}
-Requires: glib-2.0 gobject-2.0
+Requires: glib-2.0 gobject-2.0 hiredis gee-0.8
